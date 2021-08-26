9383f4d6cccbe5231a556e88b262194165c76bfb
Maria Beatriz Mota <mbeatriz.mbia@gmail.com>
2021-08-26 08:28:51 -0300

    Aprendendo_Float-(java)

---
+++ Float.java
@@ -0,0 +1,14 @@
+public class Float {
+
+	public static void main(String[] args) {
+	
+		float pi = 3.14f;
+		int r = 2;
+		System.out.println("o valor de pi vezes o raio: " + pi + "*" + r + " é: ");
+		float pierre = (float)(pi * r);
+		System.out.println("o valor de pi vezes o raio: " + pierre);
+	
+	
+	}
+}
+
