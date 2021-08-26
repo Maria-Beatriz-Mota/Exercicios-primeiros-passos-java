df2c680a2b46722017d11329e4681147455e6ac3
Maria Beatriz Mota <mbeatriz.mbia@gmail.com>
2021-08-26 08:28:12 -0300

    Utilizando_Double-(java)

---
+++ Numeros_Com_Virgula.java
@@ -0,0 +1,11 @@
+public class Numeros_Com_Virgula {
+
+	public static void main(String[] args) {
+			
+		double Salário = 1200.30;
+	
+			System.out.println(" o valor do seu salário é: " + Salário +"R$");
+			
+		}
+	}
+	
